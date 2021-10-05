e8df593bc7840f4bced3094f68f8e469a174510c
dtdat1102 <70246341+dtdat1102@users.noreply.github.com>
2021-10-04 21:07:26 -0700

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Em à!',
+    introDesc: `Anh biết anh lúc trước 
+    là 1 thằng không thể bảo vệ được em
+    nhưng mà bây giờ anh đã trưởng thành hơn 
+    và có thể bảo vệ được em`,
+    btnIntro: 'hihi',
+    title: 'Bé quay lại với anh nha😙',
+    desc: 'Bé là nhất trong trái tim anh ',
+    btnYes: 'Dạ',
+    btnNo: 'Không:<',
+    question:'Anh sẽ yêu em nhiều ơi là nhiều sẽ không bỏ bê bé nữa <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Bé là 1 phần trong cuộc sống của anh rồi nên anh không thể xa bé được. <33333333',
+    mess: 'Công chúa của anh là số 1 😘😘',
+    messDesc: 'Cho anh 1 buổi được đưa em đi chơi nha.',
+    btnAccept: 'Yêu bé <3',
+    messLink: 'https://www.facebook.com/datreal11' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
